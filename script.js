493f82e0d29d7228bf2a2cb4841b847567334b09
jose-pedro-souza <josepedrobsl22@gmail.com>
2026-09-18 14:33:03 -0300

    Enhance calculation and add input cleaning feature
    Added functionality to clean input fields and calculate reduced ICMS value.

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
+const valorVenda = document.getElementById("valorVenda");
+const valorICMS = document.getElementById("valorICMS");
 const valorFinal = document.getElementById("valorFinal");
-
 const button = document.getElementById("calculate");
+const valorReduzido = document.getElementById("valorReduzido");
 
+const cleanBtn = document.getElementById("cleanBtn");
+
+// Função para limpar os inputs
+function clean() {
+    valorVenda.value = "";
+    valorICMS.value = "";
+}
+
+cleanBtn.addEventListener("click", clean);
+
+// Botão de calcular
 button.addEventListener("click", function(event) {
     event.preventDefault();
-    const valorVenda = parseFloat(document.getElementById("valorVenda").value);
-    const valorICMS = parseFloat(document.getElementById("valorICMS").value);
 
-    function ValorFinal(valorVenda, valorICMS) {
-        return valorVenda - (valorICMS * 0.7);
+    // Pegando o valor digitado e transformando em número
+    const valorVendaNumerico = parseFloat(valorVenda.value);
+    const valorICMSNumerico = parseFloat(valorICMS.value);
+
+    function ValorFinal(a, b) {
+        return a - (b * 0.7);
     }
-    valorFinal.value = ValorFinal(valorVenda, valorICMS);
-})
 
+    valorFinal.value = ValorFinal(
+        valorVendaNumerico,
+        valorICMSNumerico
+    ).toFixed(2);
+
+    valorReduzido.value = (valorICMSNumerico * 0.7).toFixed(2);
+});
